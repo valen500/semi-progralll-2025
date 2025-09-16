ce4f73f2fe22c110b1e6f69fa1202a50f56c3430
valen500 <brianalvarado078@gmail.com>
2025-09-16 08:06:06 -0600

    Enviar datos desde el cliente al servidor

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib import parse
 
 class Servidor(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -13,4 +14,15 @@ class Servidor(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"404 - No encontrado")
 
+    def do_POST(self):
+        longitud = int(self.headers['Content-Length'])
+        datos = self.rfile.read(longitud)
+        datos = datos.decode("utf-8")
+        datos = parse.unquote(datos)
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(datos.encode("utf-8"))
+
+print("Servidor ejecutándose en el puerto 3000")
 HTTPServer(("localhost", 3000), Servidor).serve_forever()
